0db5c83072ff507f58295afbaf0f8a22471fd0e5
Matul Jain <matul3jan@gmail.com>
2023-03-09 10:29:57 +0000

    Persist locally using redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,15 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 const enhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, enhancers);
+const persistConfig = { key: "root", storage: storage, blacklist: ["user"] };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, undefined, enhancers);
+export const persistor = persistStore(store);
